92146e0c63fce12df7a811c0585801dbd8adc053
VictorDergunov <90251734+VictorDergunov@users.noreply.github.com>
2022-06-20 23:49:48 +0300

    Сделали реализацию сохраниние данных при пересоздание активити

--- app/src/main/java/com/example/android_studio_lesson3/MainActivity.java
+++ app/src/main/java/com/example/android_studio_lesson3/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.android_studio_lesson3;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_NUMBER = "key_number";
+
+    private EditText numberEditText;
+    private Button operationButton;
+    private TextView resultTextView;
+    private int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_NUMBER)){
+            number = savedInstanceState.getInt(KEY_NUMBER);
+        }
+
+        showText();
+
+        operationButton.setOnClickListener(v -> {
+            number = Integer.parseInt(String.valueOf(numberEditText.getText()));
+            number = number * number;
+            showText();
+        });
+    }
+
+    private void initView() {
+        resultTextView = findViewById(R.id.result_text_view);
+        operationButton = findViewById(R.id.operation_button);
+        numberEditText = findViewById(R.id.number_edit_text);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_NUMBER, number);
+    }
+
+    private void showText() {
+        resultTextView.setText(String.valueOf(number));
     }
 }
